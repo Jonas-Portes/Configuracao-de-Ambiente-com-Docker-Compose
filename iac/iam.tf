7b1c4e86eb3c9dee99612530550a05b836c93cfa
Jonas Portes <jonas.portes@hotmail.com>
2026-04-30 15:15:17 -0300

    fix: fix ci

---
+++ iac/iam.tf
@@ -0,0 +1,75 @@
+resource "aws_iam_openid_connect_provider" "oidc-git" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+
+
+  #thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"]
+
+
+
+  tags = {
+    IAC = "True"
+  }
+
+}
+
+resource "aws_iam_role" "ecr-role" {
+  name = "ecr-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::295783976120:oidc-provider/token.actions.githubusercontent.com"
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : [
+              "sts.amazonaws.com"
+            ]
+          },
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:Jonas-Portes/DockerCompose.ci.api:ref:refs/heads/master"
+            ]
+          }
+        }
+      }
+    ]
+  })
+
+  inline_policy {
+    name = "erc-app-permission"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          "Sid" : "Statement1",
+          "Effect" : "Allow",
+          "Action" : [
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:BatchGetImage",
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:PutImage",
+            "ecr:InitiateLayerUpload",
+            "ecr:UploadLayerPart",
+            "ecr:CompleteLayerUpload",
+            "ecr:GetAuthorizationToken",
+          ],
+          "Resource" : "*"
+        },
+      ]
+    })
+  }
+
+  tags = {
+    IAC = "true"
+  }
+}
